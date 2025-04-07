3c38e224a9234069556bc8abf7563c93b213e558
David Schol <d.schol@fontys.nl>
2025-04-07 22:23:17 +0200

    Arial as font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,6 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "./globals.css";
-import { Poppins } from "next/font/google";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  style: ["normal", "italic"],
-  display: "swap",
-});
 
 export const metadata = {
   title: "Next.js - Dinsdag",
@@ -17,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={poppins.className}>
+    <html lang="en">
       <body className="bg-gray-100 text-gray-900 max-w-7xl mx-auto mt-10">
         <Header />
         {children}
